9513734faea5f2eca63bae4ddec08d5ad148fd0c
user <user@Mac.local>
2017-03-18 15:35:52 +0800

    first commit

---
+++ HelloSwiftNavigation/VC3.swift
@@ -0,0 +1,35 @@
+//
+//  VC3.swift
+//  HelloSwiftNavigation
+//
+//  Created by user on 2017/3/9.
+//  Copyright © 2017年 HeartCoding. All rights reserved.
+//
+
+import UIKit
+
+class VC3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
