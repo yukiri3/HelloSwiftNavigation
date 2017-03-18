ffbdea515cbfa52bd208411e8de238802c08a481
user <user@Mac.local>
2017-03-18 15:37:59 +0800

    update

--- HelloSwiftNavigation/VC3.swift
+++ HelloSwiftNavigation/VC3.swift
@@ -13,23 +13,17 @@ class VC3: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func gogoBack(_ sender: Any) {
+       //回到navigation中的指定頁面
+       _ = self.navigationController?.popToViewController((self.navigationController?.viewControllers[1])!, animated: true)
     }
-    */
 
 }
